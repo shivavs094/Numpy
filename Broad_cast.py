457df2d4d1a5db9d63dcf08f95d5b7c39e86d5fb
Shiva <108185580+shivavs094@users.noreply.github.com>
2022-07-14 22:14:54 +0530

    Add files via upload

---
+++ Broad_cast.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=np.array([[1,2],[3,4],[5,6]])
+print(a)
+b=np.array([25,30])
+c=a+b
+print(c)
+
+d=np.array([[1],[2],[3]])
+e=np.array([1,2,3])
+print(d.shape)
+print(e.shape)
+f=d+e
+print(f)
+
