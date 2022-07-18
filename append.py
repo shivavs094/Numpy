39c63a6922c45ee7748d2a0e0988ba9a9b2c9ab3
Shiva <108185580+shivavs094@users.noreply.github.com>
2022-07-18 23:40:41 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=np.arange(1,20,2)
+b=np.append(a,20.20)
+print(b)
+
+
+c=np.array([[1,2,25],[3,4,26]])
+d=np.append(c,[[4,5,27]],axis=0)
+print(d)
+
+d=np.append(a,[[4],[5],[6]])
+print(e)
+
+
